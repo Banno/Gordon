fee530a025011cfc6eac17df9e5029ec25a398d1
Joshua Schriever <joshua.schriever@banno.com>
2020-06-16 13:07:33 -0500

    update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+plugins {
+    kotlin("jvm") apply false
+    kotlin("android") apply false
+    kotlin("android.extensions") apply false
+    kotlin("plugin.serialization") apply false
+    id("org.jmailen.kotlinter") apply false
+    id("com.android.application") apply false
+}
+
 tasks.register<Delete>("clean") {
     delete(buildDir)
 }
