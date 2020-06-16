0a728f769721940670089ea4de0f7fbad97c3705
Joshua Schriever <joshua.schriever@banno.com>
2020-06-16 13:31:34 -0500

    update androidx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,9 +28,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk7"))
 
     implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.1.0")
+    implementation("androidx.core:core-ktx:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("com.google.android.material:material:1.0.0")
+    implementation("com.google.android.material:material:1.1.0")
 
     androidTestImplementation("androidx.test:runner:1.2.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
