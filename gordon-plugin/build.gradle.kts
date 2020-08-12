b101c93697129682e2f1bdae0e8c35848526985f
Joshua Schriever <joshua.schriever@banno.com>
2020-08-12 10:43:24 -0500

    update plugin-publish

--- gordon-plugin/build.gradle.kts
+++ gordon-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
-    id("com.gradle.plugin-publish") version "0.11.0"
+    id("com.gradle.plugin-publish") version "0.12.0"
     `maven-publish`
     kotlin("plugin.serialization")
     id("org.jmailen.kotlinter")
